a58f51b5f87c7c4af602920fea490fb8edd1760b
Chauptm <71380488+Chauptm@users.noreply.github.com>
2021-04-29 23:06:37 +0700

    Add files via upload

---
+++ Lab4/framework/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/** Configuration Variables **/
+
+define ('DEVELOPMENT_ENVIRONMENT',true);
+
+
+define('DB_NAME', 'framework');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '123456789');
+define('DB_HOST', 'localhost:3300');
+
+define('BASE_PATH','http://localhost/framework');
+
+
+define('PAGINATE_LIMIT', '5');
